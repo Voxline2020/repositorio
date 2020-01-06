f8613303dbf07fb87c9b91daef50c377b3bf3749
brodez <brandon.h.cofre@gmail.com>
2020-01-06 10:23:50 -0300

    se borra deleted_at model del content

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -33,8 +33,6 @@ class Content extends Model
   const CREATED_AT = 'created_at';
   const UPDATED_AT = 'updated_at';
 
-  protected $dates = ['deleted_at'];
-
   public $fillable = [
     'name',
     'location',
