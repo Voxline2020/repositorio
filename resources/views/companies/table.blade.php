244fa8bbdbf50ca0310ede405747906ba2d336ae
brodez <brandon.h.cofre@gmail.com>
2020-02-14 18:25:05 -0300

    CompanyCorrection

--- resources/views/companies/table.blade.php
+++ resources/views/companies/table.blade.php
@@ -1,40 +1,3 @@
-{{-- @foreach ($companies as $company)
-<div class="col-md-4 p-1">
-	<div class="card">
-		<img src="https://picsum.photos/400/200" class="card-img-top" alt="...">
-		<div class="card-body">
-			<h5 class="card-title">{{ $company->name }}</h5>
-			<div class="row p-0 m-0">
-				<div class="col-lg-6 py-1 px-1">
-					<a href="{{ route('companies.stores.index', [$company]) }}" class="btn btn-success w-100">Sucursales</a>
-				</div>
-				<div class="col-lg-6 py-1 px-1">
-					<a href="#" class="btn btn-danger w-100">Incidencias</a>
-				</div>
-				<div class="col-lg-6 py-1 px-1">
-					<a href="{{ route('companies.events.index', [$company]) }}" class="btn btn-warning w-100">Eventos</a>
-				</div>
-				<div class="col-lg-6 py-1 px-1">
-							<a href="{{ route('computers.index', [$company]) }}" class="btn btn-primary w-100">Computadores</a>
-					</div>
-					<div class="col-lg-6 py-1 px-1">
-						<a href="{{ route('pivots.index', [$company]) }}" class="btn btn-dark w-100"><i class="fas fa-server"></i></a>
-					</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endforeach --}}
-
-{{-- @if(Auth::user()->hasRole('Administrador'))
-<div class="col-md-4 p-1">
-	<div class="card h-100 ">
-		<a href="{!! route('companies.create') !!}"
-			class="btn btn-success w-100 h-100 d-flex justify-content-center align-items-center font-weight-bolder"
-			style="font-size:3rem">+</a>
-	</div>
-</div>
-@endif --}}
 <div class="table-responsive">
 	<table class="table table-hover">
 		<thead class="thead-dark">
@@ -63,15 +26,25 @@
 					$pivotCount=$pivotCount+1;
 				}
 			}
+
+			$storeCount = 0;
+			if($company->stores != null){
+				$storeCount = $company->stores->count();
+			}
+
+			$eventCount = 0;
+			if($company->events != null){
+				$eventCount = $company->events->count();
+			}
 			@endphp
 			<tr>
 				<td>{!! $company->name !!}</td>
-				<td style="text-align: center" class="{!! $company->stores->count() == 0 ? 'red-text': '' !!}">
-					{!! $company->stores->count() !!}
+				<td style="text-align: center" class="{!! $storeCount== 0 ? 'red-text': '' !!}">
+					{!! $storeCount !!}
 				</td>
 				{{-- <td style="text-align: center; color: red;">1</td> --}}
-				<td style="text-align: center" class="{!! $company->events->count() == 0 ? 'red-text': '' !!}">
-					{!! $company->events->count() !!}
+				<td style="text-align: center" class="{!! $eventCount == 0 ? 'red-text': '' !!}">
+					{!! $eventCount !!}
 				</td>
 				<td style="text-align: center" class="{!! $computersCount == 0 ? 'red-text': '' !!}">
 					{!! $computersCount !!}
