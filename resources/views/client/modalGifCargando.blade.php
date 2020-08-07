4d1c815d90d8c0aba8ca0bc183bb61e6a4088e97
Voxline2020 <daniel@voxline.cl>
2020-08-07 15:33:03 -0400

    modificacion index blade include modals

---
+++ resources/views/client/modalGifCargando.blade.php
@@ -0,0 +1,11 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="giftModal" data-backdrop="static" data-keyboard="false"> <!-- modal gift cargando -->
+  <div class="modal-dialog" role="document" >
+    <div class="modal-content" style="background-color: rgba(0,0,0,.0001); border: 0;" >
+      
+      <div class="modal-body">
+        <img src="https://acegif.com/wp-content/uploads/loading-13.gif" width="357" height="357" style="position: absolute; top: 0px; left: 0px;">
+      </div>
+      
+    </div>
+  </div> <!-- fin modal dialog -->
+</div> 
