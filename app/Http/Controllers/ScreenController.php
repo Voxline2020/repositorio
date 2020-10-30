cf5aa9e82d3cedaa420aa36eb43d5cd23818f89e
voxline <voxline2020@github.com>
2020-10-30 12:34:52 -0300

    url getdeviceversion 9

--- app/Http/Controllers/ScreenController.php
+++ app/Http/Controllers/ScreenController.php
@@ -288,7 +288,7 @@ class ScreenController extends AppBaseController
   public function apiGetDeviceVersion(Request $request, $code, $key)
 	{
     $device = Device::where('code' ,'=', $code);
-    return $device;
+    return $device->toString();
     $jsonResponse = [];
 		$jsonResponse['version'] = json_encode($device);
     return json_encode($device);
